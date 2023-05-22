5c2b2939ea628eff8dd4994e09cf876959f1974a
genesischavez2 <127029593+genesischavez2@users.noreply.github.com>
2023-05-22 17:51:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int d;
+  cin>>d;
+  float numeri[d];
+  for(int i=0;i<d;i++){
+    cin>>numeri[i];
+  }
+  cout << funz(numeri,d)<<endl;
   return 0;
 }
